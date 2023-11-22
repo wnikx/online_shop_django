05b48c5ceba0b1b71dce4123cea3a0381d08d7a5
Nikita Ostapenko <wnikx79@gmail.com>
2023-11-22 15:33:16 +0300

    add products_categories.html

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from shop import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.main_page)
+    path('', views.main_page),
+    path('products_categories/', views.products_categories,
+         name='products_categories'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from shop.models import Category, Product
 
 
 def main_page(request):
-    return render(request, 'shop/base.html')
+    return render(request, 'base.html')
+
+
+def products_categories(request):
+    products_categories = Category.objects.all()
+    return render(request, 'shop/products_categories.html', context={'categories': products_categories})
